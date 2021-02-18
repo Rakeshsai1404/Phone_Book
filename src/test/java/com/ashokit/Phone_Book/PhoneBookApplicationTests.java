ae7216016330f7fa0db334e3b02f6d3fe2abed7b
Rakeshsai1404 <rakeshkumar14495@gmail.com>
2021-02-18 09:49:58 +0530

    My first commit

---
+++ src/test/java/com/ashokit/Phone_Book/PhoneBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashokit.Phone_Book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
